9d6622d72a71a4d4033c50e279526f9c3397bf3b
Mr X <mrx@Mrs-MacBook-Pro.local>
2017-03-05 21:35:35 +0100

    Almost finnished

--- SwiftLabb1/CompareViewController.swift
+++ SwiftLabb1/CompareViewController.swift
@@ -23,9 +23,10 @@ class CompareViewController: UIViewController{
         name2.text = food2.name!
         name1.textColor = UIColor.blue
         name2.textColor = UIColor.green
-        let graph = GKBarGraph(frame: CGRect(x: view.frame.minX, y: view.frame.minY, width: view.frame.width, height: view.frame.height * 0.8))
-        graph.barWidth = 15
-        graph.barWidth = 10
+        let graph = GKBarGraph(frame: CGRect(x: view.frame.minX, y: view.frame.height/12, width: view.frame.width, height: view.frame.height * 0.8))
+        graph.barWidth = 30
+        graph.marginBar = 15
+        graph.barHeight = 90
         let customData1 = CustomBarGraph(food1: food1, food2: food2)
         graph.dataSource = customData1
         view.addSubview(graph)
